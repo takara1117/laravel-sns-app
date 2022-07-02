125a4c2aa3268dfa350c1c2bb257e046d7e2f28d
takara1117 <74242876+takara1117@users.noreply.github.com>
2022-07-02 17:21:31 +0900

    login機能編集

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,95 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+	<div class="forms">
+		<div class="round">
+			<img src="{{ asset('images/Instagram_logo.png')}}">
+			<form action="#">
+				<div>
+					<label for="mail">
+						<input type="email" placeholder="メールアドレス" class="mail" required>
+					</label>
+				</div>
+				<div>
+					<label for="pass">
+						<input type="password" placeholder="パスワード" class="pass" required>
+					</label>
+				</div>
+				
+
+				<div class="submit">
+					<input type="submit" class="send" value="ログイン">
+				</div>
+			</form>
+		</div>
+	</div>
+	<div class="account_registration">
+		<div class="round">
+			<p class="sentence">アカウントをお持ちでないですか？</p>
+			<a href="#" class="registration">登録する</a>
+		</div>
+	</div>
+</body>
+</html>
+
+<style>
+	body {
+		width: 100%;
+		max-width: 1280px;
+		margin: auto;
+		background: #f5f5f5;
+	}
+	.round {
+		padding: 20px;
+		border: 2px solid #dcdcdc;
+		background: #ffffff;
+	}
+	.forms {
+		text-align:center;
+		padding-top: 25%;
+	}
+	img {
+		width: 18%;
+	}
+	div {
+		padding-top: 2%;
+	}
+	.mail {
+		width: 25%;
+		height: 4%;
+	}
+	.pass {
+		width: 25%;
+		height: 4%;
+	}
+	.send {
+		width: 25%;
+		height: 4%;
+		color: #ffffff;
+		font-size: 1.2em;
+		font-weight: bold;
+		background-color: #1e90ff;
+		border: none;
+		border-radius: 7px;
+		cursor: pointer;
+	}
+	.account_registration {
+		text-align:center;
+		margin: auto;
+		padding-top: 1%;
+	}
+	.sentence {
+		font-size: 1.1em;
+	}
+	.registration {
+		font-size: 1.2em;
+		font-weight: bold;
+		color: #1e90ff;
+		text-decoration: none;
+		cursor: pointer;
+	}
+</style>
