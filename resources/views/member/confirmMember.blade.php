b801589234a602fad4af8b055cd9eb177a389c68
takara1117 <74242876+takara1117@users.noreply.github.com>
2022-07-03 14:22:40 +0900

    confirmMember 途中

---
+++ resources/views/member/confirmMember.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>入力内容確認画面</title>
+</head>
+<body>
+	<p>入力内容確認画面</p>
+</body>
+</html>
