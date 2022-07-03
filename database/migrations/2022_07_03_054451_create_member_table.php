4db465d647e72f09e951372b96791e62e7630621
takara1117 <74242876+takara1117@users.noreply.github.com>
2022-07-03 15:34:10 +0900

    db作成途中

--- database/migrations/2022_07_03_054451_create_member_table.php
+++ database/migrations/2022_07_03_054451_create_member_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('member', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('tel');
+            $table->string('email')->unique();
+            $table->string('password');
             $table->timestamps();
         });
     }
